0382f963233825ad3bc32f629a2c3da8af4fd68f
Sangeetha Chandrashekar <ch.sangtha@gmail.com>
2024-06-17 20:40:55 -0400

    completed precourse-1

--- Exercise_1.js
+++ Exercise_1.js
@@ -1,35 +1,60 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Not tested on leetcode
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+//Stack implementation - LIFO: Last In First Out linear data structure using Array in JS
 class Stack {
-  //Please read sample.java file before starting.
-  //Kindly include Time and Space complexity at top of each file
-​
-    constructor() {
-        //Initialize your constructor
-        this.MAX = 1000;
-        this.top = -1;
-        this.a = new Array(this.MAX);
-    }
-​
-    function isEmpty() {
-        //Write your code here
-    }
-​
-    function push(x) {
-        //Check for stack Overflow
-        //Write your code here
-    }
-​
-    function pop() {
-        //If empty return 0 and print " Stack Underflow"
-        //Write your code here
-    }
-​
-    function peek() {
-       //Write your code here
-    }
-}
-​
-let s = new Stack();
-s.push(10);
-s.push(20);
-s.push(30);
-console.log(s.pop() + " Popped from stack");
+    //Please read sample.java file before starting.
+    //Kindly include Time and Space complexity at top of each file
+      constructor() {
+          //Initialize your constructor
+          this.MAX = 2;
+          this.top = -1;
+          this.a = new Array(this.MAX);
+      }
+      isEmpty() {
+          //Write your code here
+          var isEmpty = (this.top < 0);
+          return isEmpty;
+      }
+      push(x) {
+          //Check for stack Overflow
+          //Write your code here
+          if(this.top >= this.MAX-1)
+              console.log('Stack Overflow');
+          else {
+              this.top++;
+              this.a[this.top] = x;
+              console.log(`${x} pushed into stack`);
+          }
+      }
+      pop() {
+          //If empty return 0 and print " Stack Underflow"
+          //Write your code here
+          if(this.top < 0){
+              console.log('Stack Underflow');
+              return 0;
+          }
+          else {
+              var popVal = this.a[this.top];
+              this.top--;
+              return popVal;
+          }
+      }
+      peek() {
+         //Write your code here
+         if(this.top < 0)
+              console.log('Stack Underflow');
+         else {
+              return this.a[this.top];
+         }
+      }
+  }
+  var s = new Stack();
+  s.push(10);
+  s.push(20);
+  s.push(30);
+  console.log(s.pop() + " Popped from stack");
--- Exercise_2.js
+++ Exercise_2.js
@@ -1,30 +1,58 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Not tested on leetcode
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
 class StackAsLinkedList {
-​
+    constructor() {
+        this.head = null;
+    }
+
     static stackNode = class {
-​
         constructor(d) {
             //Constructor here
             this.data = d;
             this.next = null;
         }
     }
-​
-    function isEmpty() {
+    isEmpty() {
         //Write your code here for the condition if stack is empty.
+        if (this.head == null)
+            return true;
+        return false;
     }
-​
-    function push(data) {
-       //Write code to push data to the stack.
+    push(data) {
+        //Write code to push data to the stack.
+        let newNode = new StackAsLinkedList.stackNode(data);
+        if (this.head == null)
+            this.head = newNode;
+        else {
+            let curHeadNode = this.head;
+            this.head = newNode;
+            newNode.next = curHeadNode;
+        }
+        console.log(`${data} pushed to stack`);
     }
-​
-    function pop() {
-       //If Stack Empty Return 0 and print "Stack Underflow"
-       //Write code to pop the topmost element of stack.
-       //Also return the popped element
+    pop() {
+        //If Stack Empty Return 0 and print "Stack Underflow"
+        //Write code to pop the topmost element of stack.
+        //Also return the popped element
+        if (this.head == null)
+            console.log('Stack Underflow');
+        else {
+            var node = this.head;
+            this.head = node.next;
+            return node.data;
+        }
     }
-​
-    function peek() {
-       //Write code to just return the topmost element without removing it.
+    peek() {
+        //Write code to just return the topmost element without removing it.
+        if (this.head == null)
+            console.log('Stack Underflow');
+        else
+            return this.head.data;
     }
 }
 //Driver code
